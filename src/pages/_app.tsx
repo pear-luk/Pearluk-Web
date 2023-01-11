d64f40ef0d03134ac98fa99a4d9f869f54bc1b16
Denovo <vo0v0000@naver.com>
2023-01-12 02:49:48 +0900

    [ADD] eslint, prettier, storybook 세팅 #1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import GlobalStyle from '@src/styles/globalStyle';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
